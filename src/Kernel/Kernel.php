7d1e5986fecead253c01560261631e6bc7fdaea8
Le Ngan <le.ngan@lundimatin.fr>
2024-09-09 11:05:56 +0700

    refactor routing (WIP)

--- src/Kernel/Kernel.php
+++ src/Kernel/Kernel.php
@@ -5,19 +5,13 @@ namespace App\Ecommerce\Kernel;
 class Kernel
 {
     function run($interface) {
-        $controller = $_GET["controller"] ?? "";
-        $action = $_GET["action"] ?? "";
 
-        if (empty($controller) && empty($action)) {
-            if ($interface == 'front') {
-                $controller = "front.home";
-            } elseif ($interface == 'admin') {
-                $controller = "admin.home";
-            }
-            $action = "index";
-        }
         $router = new Router();
-        $callback = $router->routing($controller, $action);
+        $callback = $router->routing();
+        if (empty($callback)) {
+            throw new \Exception("Route invalid");
+        }
+
         $class = $callback["class"];
         $method = $callback["function"];
 
--- src/Kernel/Router.php
+++ src/Kernel/Router.php
@@ -1,33 +1,68 @@
 <?php
+
 namespace App\Ecommerce\Kernel;
 
 class Router
 {
-    public function routing($controller, $action)
+    public function routing()
     {
-        $mapping = [
-            "admin.home" => [
-                "index" => [
-                    "class" => \App\Ecommerce\Controller\Admin\HomeController::class,
-                    "function" => "index"
-                ]
-            ],
+        $uri = $_SERVER['REQUEST_URI'];
+        $method = $_SERVER['REQUEST_METHOD'];
+        $mapping = $this->getMapping();
 
-            "front.home" => [
-                "index" => [
-                    "class" => \App\Ecommerce\Controller\Front\HomeController::class,
-                    "function" => "index"
-                ]
-            ]
-        ];
-        
-        if (empty($mapping[$controller][$action])) {
-            throw new \Exception("Route not found : controller $controller, action $action");
+        foreach ($mapping as $item) {
+            if ($method !== $item['method']) {
+                continue;
+            }
+
+            $route = $item['route'];
+            if ($this->match($route, $uri)) {
+                return $route;
+            }
         }
-        return $mapping[$controller][$action];
+
+        return null;
     }
 
-    public function redirect($controller, $action) {
+    private function getMapping()
+    {
+        return [
+            [
+                "route" => "admin",
+                "method" => "GET",
+                "class" => \App\Ecommerce\Controller\Admin\HomeController::class,
+                "function" => "index"
+            ],
+            [
+                "route" => "admin/product/{id}",
+                "method" => "GET",
+                "class" => \App\Ecommerce\Controller\Admin\ProductController::class,
+                "function" => "viewProduct"
+            ],
+
+
+            [
+                "route" => "/",
+                "method" => "GET",
+                "class" => \App\Ecommerce\Controller\Front\HomeController::class,
+                "function" => "index"
+            ]
+        ];
+    }
+
+    public function redirect($controller, $action)
+    {
         header("location:index.php?controller=$controller&action=$action");
+    }
+
+    private function match(string $route, string $uri)
+    {
+        $route = '/' . trim($route, '/');
+        $uri = rtrim($uri, '/');
+
+        if(preg_match("/\{([^{}]+)}/i", $route, $matches)) {
+            // todo
+        }
+
     }
 }
