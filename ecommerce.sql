c76475251c835deee2dd1fbbe52070ba46bffe62
hien <thuhien33qt@gmail.com>
2024-09-13 08:47:22 +0700

    add file sql  and change db

---
+++ ecommerce.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE IF NOT exists ecommerce;
+
+USE ecommerce;
+
+CREATE TABLE IF NOT exists user (
+    userID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    username VARCHAR(255),
+    password VARCHAR(255),
+    address VARCHAR(255),
+    phone INT,
+    role enum("admin", "user"),
+    UNIQUE (username)
+);
+
+CREATE TABLE IF NOT exists category (
+    categoryID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT exists products (
+    productID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    categoryID INT,
+    price VARCHAR(255),
+    imgURL VARCHAR(255),
+    description VARCHAR(255),
+    CONSTRAINT FOREIGN KEY (categoryID) REFERENCES category (categoryID)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE IF NOT exists `order` (
+    orderID INT PRIMARY KEY AUTO_INCREMENT,
+    productID INT,
+    quantity INT,
+    unitPrice INT,
+    userID INT,
+    date DATETIME,
+    CONSTRAINT FOREIGN KEY (productID) REFERENCES products (productID)
+    ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT FOREIGN KEY (userID) REFERENCES user (userID)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE IF NOT exists chat (
+    chatID INT PRIMARY KEY AUTO_INCREMENT,
+    userID INT,
+    productID INT,
+    message VARCHAR(255),
+    date DATETIME,
+    CONSTRAINT FOREIGN KEY (productID) REFERENCES products (productID)
+    ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT FOREIGN KEY (userID) REFERENCES user (userID)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE IF NOT exists payment (
+    paymentID INT PRIMARY KEY AUTO_INCREMENT,
+    orderID INT,
+    amount INT,
+    date DATETIME,
+    method enum("banking transfer", "cash"),
+    CONSTRAINT FOREIGN KEY (orderID) REFERENCES `order` (orderID)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+
+INSERT INTO user (name, username, password, role) 
+    VALUES 
+        ("Karl", "karl", "1990", "admin"), 
+        ("Jack", "jack", "2000", "user"), 
+        ("Anthony", "anthony", "1998", "user"), 
+        ("Amy", "amy", "1990", "user");
+
+
+INSERT INTO category (name) 
+    VALUES 
+        ("aqua"), 
+        ("panasonic");
+
+INSERT INTO products (name, categoryID, price, imgURL, description) 
+    VALUES 
+        ("điều hoà", "1", "1990", "abc", "ok"), 
+        ("điều hoà", "2", "1990", "abc", "ok"), 
+        ("máy lọc nước", "1", "1990", "abc", "ok"), 
+        ("máy lọc nước", "2", "1990", "abc", "ok")
--- src/Model/Product.php
+++ src/Model/Product.php
@@ -2,6 +2,20 @@
 namespace App\Ecommerce\Model;
 class Product {
     private $name;
+    private $category;
+    private $price;
+    private $imgURL;
+    private $description;
+
+    public function __construct($data)
+    {
+        $this->setName($data->name);
+        $this->setCategory($data->category);
+        $this->setPrice($data->price);
+        $this->setImgURL($data->imgURL);
+        $this->setDescription($data->description);
+    }
+
     public function getName()
     {
         return $this->name;
@@ -10,5 +24,45 @@ class Product {
     public function setName($name)
     {
         $this->name = $name;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getImgURL()
+    {
+        return $this->imgURL;
+    }
+
+    public function setImgURL($imgURL)
+    {
+        $this->imgURL = $imgURL;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
     }
 }
